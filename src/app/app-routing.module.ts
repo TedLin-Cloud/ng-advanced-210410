07ea50af14857d96bc06fd8321ea76d781df279f
林萬庭 <tf625585@gmail.com>
2021-04-10 16:56:04 +0800

    auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './auth.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -5,25 +6,35 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { ColorComponent } from './utilities/color/color.component';
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
+// import { QuicklinkModule, QuicklinkStrategy } from 'ngx-quicklink';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path: 'page1', component: Page1Component },
-  { path: 'page2', component: Page2Component },
-  { path: 'dashboard', component: DashboardComponent },
   {
-    path: 'utilities', children: [
-      { path: 'color', component: ColorComponent, data: { seo: "111" } },
-      { path: 'color/:type', component: ColorComponent , data: { seo: "111" }}
+    path: '', component: LayoutComponent ,children: [
+      { path: '', component: DashboardComponent },
+      { path: 'page1', component: Page1Component },
+      { path: 'page2', component: Page2Component, canActivate: [AuthGuard] },
+      { path: 'dashboard', component: DashboardComponent },
+      {
+        path: 'utilities', children: [
+          { path: 'color', component: ColorComponent, data: { seo: "111" } },
+          { path: 'color/:type', component: ColorComponent, data: { seo: "111" } }
+        ]
+      },
+      { path: 'components', loadChildren: () => import('./components/components.module').then(m => m.ComponentsModule) },
     ]
   },
-  { path: 'components', loadChildren: () => import('./components/components.module').then(m => m.ComponentsModule) },
+  { path: 'login', component: LoginComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    useHash: true
+    useHash: true,
+    //QuicklinkModule,
+    //preloadingStrategy:QuicklinkStrategy
   })],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { Page2Component } from './page2/page2.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ColorComponent } from './utilities/color/color.component';
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { ColorComponent } from './utilities/color/color.component';
     Page2Component,
     PageNotFoundComponent,
     DashboardComponent,
-    ColorComponent
+    ColorComponent,
+    LoginComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
